0cbadbbe2ca321dc715c016ef2b1ce045c7a1cda
Arya Anish <anishkumarsarmi@gmail.com>
2025-05-22 21:46:50 +0530

    make better

--- commands.md
+++ commands.md
@@ -1,3 +1,7 @@
-```
-ffmpeg -i bad_apple.mp4 -c copy -t 10 output.mp4
+```bash
+ffmpeg -i bad_apple.mp4 -c copy -t 10 input.mp4
+python main.py
+ffmpeg -framerate 30 -pattern_type glob -i '*.jpg' \
+  -c:v libx264 -pix_fmt yuv420p out.mp4
+rm -rf input_img output_img
 ```
--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import cv2
 import os
 from image_to_ascii_lib.converter import image_to_ascii, ascii_to_image
 
-def extract():
-    cam = cv2.VideoCapture("./bad_apple.mp4")
+frames = 0
 
-    if not os.path.exists('input_img'):
-        os.makedirs('input_img')
+if not os.path.exists('input_img'):
+    os.makedirs('input_img')
+
+if not os.path.exists('output_img'):
+    os.makedirs('output_img')
+
+def extract():
+    cam = cv2.VideoCapture("./input.mp4")
 
     currentframe = 0
 
@@ -15,8 +20,6 @@ def extract():
 
         if ret:
             name = './input_img/frame' + str(currentframe) + '.jpg'
-            print ('Creating...' + name)
-
             cv2.imwrite(name, frame)
 
             currentframe += 1
@@ -25,11 +28,18 @@ def extract():
 
     cam.release()
     cv2.destroyAllWindows()
+    # better way to do dis but i am lazy
+    global frames
+    frames = currentframe
+    print("extracted")
 
 def convert(inputFileName, outFileName):
     ascii_art = image_to_ascii(inputFileName, size=(30, 50))
     ascii_art_image = ascii_to_image(ascii_art)
     ascii_art_image.save(outFileName)
 
-
-convert("test.jpg", "opt.jpg")
+extract()
+for i in range(1, frames):
+    inputFileName = f"input_img/frame{i}.jpg"
+    outFileName = f"output_img/frame{i}.jpg"
+    convert(inputFileName=inputFileName, outFileName=outFileName)
